fcb0a7b66a7639a62407c53d7d48217d516422ca
Sullivan Marin Garcia <sullivan.marin20@gmail.com>
2023-04-12 11:01:25 +0100

    Añadido componente register

--- sprint3/CanaryBnB/src/app/app.module.ts
+++ sprint3/CanaryBnB/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FormsModule } from '@angular/forms'; // Asegúrate de importar FormsMod
 import { HttpClientModule } from '@angular/common/http';
 import { DisponiblesComponent } from './disponibles/disponibles.component';
 import { HotelfilterComponent } from './hotelfilter/hotelfilter.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { HotelfilterComponent } from './hotelfilter/hotelfilter.component';
     HomeComponent,
     DisponiblesComponent,
     HotelfilterComponent,
+    RegisterComponent,
    
     //DisponiblesComponent // Añade DisponiblesComponent aquí
   ],
