874dc8558f721b466c11b8e810d59d516d1dd5cf
Naim <naimsabas@hotmail.com>
2023-04-17 08:40:36 +0100

    Firebase, bootstrap, popper.js & Jquery añadidos

--- sprint3/CanaryBnB/src/app/app.component.ts
+++ sprint3/CanaryBnB/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nombre-de-tu-aplicacion';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCH_ecZzdkC0HJXy5yvMizPhGPIiXtoh6s",
+      authDomain: "canarybnb-db.firebaseapp.com",
+    });
+  }
 
   onFiltersApplied(filters: any): void {
     console.log('Filtros aplicados:', filters);
