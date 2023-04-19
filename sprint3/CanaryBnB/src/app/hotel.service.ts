3d04d57c35d875afc33cc6c82a432cf161052353
Sebastian Apostu <sebiapostu@gmail.com>
2023-04-19 11:57:40 +0200

    Añadido carga dinamica de los alojamientos mejores valorados

--- sprint3/CanaryBnB/src/app/hotel.service.ts
+++ sprint3/CanaryBnB/src/app/hotel.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,16 @@ export class HotelService {
 
   getHotelsData(): Observable<any> {
     return this.http.get<any>(this.hotelsDataUrl);
+  }
+
+  getTopRatedHotels(): Observable<any[]> {
+    return this.getHotelsData().pipe(
+      map(data => {
+        const allHotels = [...data['Gran Canaria'], ...data['Tenerife']];
+        const sortedHotels = allHotels.sort((a, b) => b.rating - a.rating);
+        return sortedHotels.slice(0, 3);
+      })
+    );
   }
 
 
