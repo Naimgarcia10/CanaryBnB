04b75b2fcbd5d5b7c58c79a81fb867c071c73b9f
Naim <naimsabas@hotmail.com>
2023-04-19 12:10:15 +0100

    prueba

--- sprint3/CanaryBnB/src/app/register/register.component.ts
+++ sprint3/CanaryBnB/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor(private fb:FormBuilder){ }
 
+  ngOnInit() {
+  }
+
+  formLogin = this.fb.group({
+    'email': ['', [Validators.required, Validators.email]],
+    'password': ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  get email(){
+    return this.formLogin.get('email') as FormControl;
+  }
+
+  get password(){
+    return this.formLogin.get('password') as FormControl;
+  }
+
+  loginAction(): void{
+    console.log(this.formLogin.value);
+  }
 }
