6524cb1b7373eb606644cb5ba6122f9d7be98fbc
Sullivan Marin Garcia <sullivan.marin20@gmail.com>
2023-04-19 11:36:34 +0100

    añadido pasarela de pago (solo el componente)

--- sprint3/CanaryBnB/src/app/register/register.component.ts
+++ sprint3/CanaryBnB/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor(private fb: FormBuilder) {}
 
+  formRegister = this.fb.group({
+    'email': ['', [Validators.required, Validators.email]],
+    'password': ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  get email() {
+    return this.formRegister.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.formRegister.get('password') as FormControl;
+  }
+
+  register() {
+    const user = {
+      username: this.email.value,
+      password: this.password.value
+    };
+    localStorage.setItem('user', JSON.stringify(user));
+    console.log('User registered:', user);
+  }
 }
